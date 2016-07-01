56e97fcb82d4737affad337dec31556e495b4d41
Tony Fabery <tfabery@gmail.com>
2016-07-01 09:26:44 -0700

    change app.rb from nonsense to fail the spec to what it should do.

--- app.rb
+++ app.rb
@@ -1,2 +1,14 @@
-<h1>Word_count</h1>
-<p>Your word was used <%= @result %> times in your sentence</p>
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+require('./lib/word_count')
+also_reload('./lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/result') do
+  @result = params.fetch('word').word_count(params.fetch('sentence'))
+  erb(:result)
+end
