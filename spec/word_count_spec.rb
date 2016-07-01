4ed3351863b4a0d7f3d798922b7c921e2e053985
Tony Fabery <tfabery@gmail.com>
2016-07-01 09:22:47 -0700

    add spec to check for count disregarding any capitalization

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,7 @@ describe('String#word_count') do
   it "takes a word and a sentence and return the number of times that the word was used in the sentence" do
     expect('this'.word_count('this sentence uses this three times to do this')).to(eq(3))
   end
+  it "takes a word and a sentence and return the number of times that the word was used in the sentence disregarding the capitalization" do
+    expect('this'.word_count('ThIS sentence uses ThIS three times to do ThIS')).to(eq(3))
+  end
 end
