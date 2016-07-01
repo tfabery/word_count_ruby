a1a43311ca9bdb41182b25d815d53e35206d80fc
Tony Fabery <tfabery@gmail.com>
2016-07-01 09:25:11 -0700

    add content to result.erb and app.rb and specs for word_count path in word_count_integration_spec.rb

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -1,1 +1,15 @@
+require('capybara/rspec')
+require("./app")
+require('launchy')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
 
+describe('word_count path', {:type => :feature}) do
+  it "processes users word nd sentence input and returns the number of time that the word was used in the sentence" do
+    visit('/')
+    fill_in('word', :with => 'This')
+    fill_in('sentence', :with => 'this sentence uses this three times to do this')
+    click_button('Count')
+    expect(page).to have_content('3')
+  end
+end
